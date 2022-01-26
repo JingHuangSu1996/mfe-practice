76a612151cbc47437d696e9e72d926be90cc3d9b
jing <jingmultiplefive@gmail.com>
2022-01-26 21:43:56 +0800

    chore: init

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import M from './components/MarketingApp';
+
+const App = () => {
+  return (
+    <div>
+      <M />
+      Hello World
+    </div>
+  );
+};
+
+export default App;
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#dev_marketing');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
