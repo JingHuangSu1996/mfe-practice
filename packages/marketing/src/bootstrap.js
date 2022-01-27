0c85bbd1a51478b881ef821d3c7a6a98c90d4b8b
jing <jingmultiplefive@gmail.com>
2022-01-27 21:50:30 +0800

    chore: test deploy

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
-};
+const mount = (el) => ReactDOM.render(<App />, el);
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#dev_marketing');
