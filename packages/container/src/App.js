d4de785aa7d796e0af583e62224b0f4b8ef19942
jing <jingmultiplefive@gmail.com>
2022-01-29 15:45:25 +0800

    fix: css bug for cross app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import M from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <M />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <M />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
